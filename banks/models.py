0977eed7fca88a777d7ebf640d23cbb4ecad047c
chiehhhaa <tomato870208@gmail.com>
2024-06-24 15:26:24 +0800

    Modify: Save CSV file to database

--- banks/models.py
+++ banks/models.py
@@ -11,10 +11,10 @@ class Bank(models.Model):
 
 class Branch(models.Model):
     bank = models.ForeignKey(Bank, related_name="branches", on_delete=models.CASCADE)
-    branch_code = models.CharField(max_length=10, unique=True)
+    branch_code = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255, blank=True)
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return f"{self.bank.bank_code} - {self.branch_code} - {self.name}"
